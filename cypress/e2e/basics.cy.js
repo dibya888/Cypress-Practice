c710eade49fabd49b6291730c6077c60e95999b4
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-01 20:08:50 +0600

    Check Logo, Get, Click, Type

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,5 +1,27 @@
+/// <reference types="Cypress" />
+
 describe("Basic Tests", () => {
-    it("Visit the website", () => {
-        cy.visit("https://www.saucedemo.com/")
+    beforeEach("Visit base website", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+    })
+    afterEach("Indicate test completion", () => {
+        cy.log("The Test is Completed.")
+    })
+
+    it("Visit the website and verify the header", () => {
+        cy.get("img[alt='company-branding']").should("be.visible")
+        cy.title().should("eq", "OrangeHRM")
+    })
+
+    // it("Select by text example", () => {
+    //     cy.visit("https://the-internet.herokuapp.com")
+    //     cy.get('a').contains("Form Authentication").click()
+    //     //cy.contains("Form Authentication").click()    //Alternate of upper line.
+    // })
+
+    it("Type Demonastration", () => {
+        cy.get('[name="username"]').type("Admin")
+        cy.get('[name="password"]').type("admin123")
+        cy.get('[type="submit"]').click()
     })
 })
