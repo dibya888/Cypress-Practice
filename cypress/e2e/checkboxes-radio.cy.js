60a4c6dca3346cc3992a858edc6468e310c06bbd
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-02 00:57:53 +0600

    Checkbox and radio button

---
+++ cypress/e2e/checkboxes-radio.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Checking checkbox", () => {
+    beforeEach('Setup', () => {
+        cy.visit('https://the-internet.herokuapp.com/checkboxes');
+    })
+
+    it("Checkbox Check", () => {
+        cy.get('#checkboxes').find('[type="checkbox"]').eq(0).check().should('be.checked');
+        cy.get('#checkboxes').find('[type="checkbox"]').eq(1).uncheck().should('not.be.checked');
+    })
+})
